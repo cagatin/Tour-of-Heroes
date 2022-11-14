6920a06d0db68078285f18f71c93f0fcd5624b87
Felix Cagatin <cagatingilbert@gmail.com>
2022-11-14 12:47:17 -0800

    Removed default template from app.component, added app-wide styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // CSS Selector that defines how the component is used in a template.
   selector: 'app-root',
+
+  // HTML template that instructs Angular on how to render the component. 
   templateUrl: './app.component.html',
+
+  // Optional set of CSS styles
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
 }
