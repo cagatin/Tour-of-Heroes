3e76f9d0849750e333e8c76d5d84636b5aed5a13
Felix Cagatin <cagatingilbert@gmail.com>
2022-11-15 12:14:54 -0800

    Added getter method to retrieve array of Heroes from hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,14 +12,23 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  // Here, we define the click event that occurs on the HeroesComponent
-  // When a hero is clicked, the selected hero is bound to the selectedHero method
+  /* Here, we define the click event that occurs on the HeroesComponent
+   * When a hero is clicked, the selected hero is bound to the selectedHero method
+   */
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  // we add the heroService to the constructor to define a rpivate heroService property
-  // When angular creates a HeroComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService
+  // getter method to retrieve heroes from the HeroService
+  getHeroes(): void {
+    // binds the Hero[] array to the result of calling herService.getHeroes();
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  /* we add the heroService to the constructor to define a private heroService property
+   * this identifies it as a HeroService injection site.
+   * When angular creates a HeroComponent, the Dependency Injection system sets the heroService parameter to the singleton instance of HeroService
+   */
   constructor(private heroService: HeroService) { }
 
   // lifecycle hook.
