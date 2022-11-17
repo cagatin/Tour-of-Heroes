08c82e6ee1ce1d4690efd75d7f0f5787f10800e0
Felix Cagatin <cagatingilbert@gmail.com>
2022-11-17 10:41:01 -0800

    Added routerLink for dashboard to route to specific hero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';             // importing routing capability
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 /* Here, we specify the routes within the Routes[] array.
  * The Routes[] array tells the Router which view to display when a user clicks a link or pastes a URL into the address bar.
@@ -9,9 +10,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
  * {path: 'string that matches URL of the path', component: <component that the router should create when navigating to the route>}
  */
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },    // here, we add a default route that points to website's route. 
   { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }        // go to specific hero detail page
 ];
 
 // Metadata that initializes the router and starts it listening for browser location changes
