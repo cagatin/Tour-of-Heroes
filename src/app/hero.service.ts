1e6919b921ca9dbd0dfdcff36e83245862a74d14
Felix Cagatin <cagatingilbert@gmail.com>
2022-11-23 08:12:14 -0800

    Added basic error handler to HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -20,7 +20,12 @@ export class HeroService {
 
   // getter which returns an array of heroes
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        // catchError() will intercept a failed Observable and pass the error to our handleError error handling funciton.
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   // getter which returns a specified hero id
@@ -42,5 +47,26 @@ export class HeroService {
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+
+  /**
+  * Handle Http operation that failed.
+  * Let the app continue.
+  *
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result
+      return of(result as T);
+    }
   }
 }
